d75f222197766d52912c773afeb82d89b600035f
satrak33 <mlord2591@gmail.com>
2025-03-13 19:15:06 +0200

    fixes

--- collect_data.py
+++ collect_data.py
@@ -1,15 +1,23 @@
 import numpy as np
 import os
 import face_recognition as fr
+from loguru import logger as log
 
+@log.catch
 def save_faces():
     """Збереження енкодингів облич із папки 'faces' у файли .npy"""
     know_face_encodings: list = []  # Список векторів облич
     know_face_names: list = []  # Список імен файлів (без розширення)
 
     for img in os.listdir('faces'):  # Перебір всіх зображень у папці 'faces'
+        log.debug(f"Processing {img}")
         face_img = fr.load_image_file(f'faces/{img}')  # Завантаження зображення
-        face_encoding = fr.face_encodings(face_img)[0]  # Отримання векторного представлення обличчя
+        try:
+            face_encoding = fr.face_encodings(face_img)[0]  # Отримання векторного представлення обличчя
+        except IndexError:
+            log.error(f"Image {img} has no face")
+
+            continue
 
         know_face_encodings.append(face_encoding)  # Додавання вектора до списку
         know_face_names.append(img[:-4])  # Видалення розширення файлу та збереження імені
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import datetime
+
 import face_recognition as fr
 import cv2
 import numpy as np
 from pathlib import Path
+from loguru import logger as log
 
 # Параметри відео та розпізнавання
-VIDEO_SOURCE = 'ch01_20250222095355.mp4'  # Джерело відео
+VIDEO_SOURCE = Path("ip")  # Джерело відео
 SCALE_FACTOR = 1  # Коефіцієнт зменшення зображення (1 - без змін)
 FRAME_SKIP = 10  # Пропуск кадрів для обробки (щоб не перевантажувати процесор)
-ROI = (800, 100, 500, 500)  # Область інтересу (ROI) у відео (x, y, width, height)
 ENCODINGS_PATH = Path("data/know_face_encodings.npy")  # Шлях до збережених енкодингів облич
 NAMES_PATH = Path("data/know_face_names.npy")  # Шлях до збережених імен
 
@@ -18,22 +20,25 @@ class FaceRecognition:
         self.known_face_names = []  # Масив імен облич
         self.load_encodings()
 
+    @log.catch
     def load_encodings(self):
         """Завантаження збережених енкодингів імен та облич."""
         self.known_face_encodings = np.load(ENCODINGS_PATH, allow_pickle=True)
         self.known_face_names = np.load(NAMES_PATH, allow_pickle=True)
 
-    def face_confidence(self, face_distance: float, face_match=0.6) -> str:
-        """Обчислення рівня впевненості у збігу обличчя."""
-        range_val = (1.0 - face_match)
-        linear_val = (1.0 - face_distance) / (range_val * 2.0)
+    # @log.catch
+    # def face_confidence(self, face_distance: float, face_match=0.6) -> str:
+    #     """Обчислення рівня впевненості у збігу обличчя."""
+    #     range_val = (1.0 - face_match)
+    #     linear_val = (1.0 - face_distance) / (range_val * 2.0)
+    #
+    #     if face_distance > face_match:
+    #         return f"{round(linear_val * 100, 2)}%"
+    #     else:
+    #         adjusted_val = linear_val + ((1.0 - linear_val) * ((linear_val - 0.5) * 2) ** 0.2)
+    #         return f"{round(adjusted_val * 100, 2)}%"
 
-        if face_distance > face_match:
-            return f"{round(linear_val * 100, 2)}%"
-        else:
-            adjusted_val = linear_val + ((1.0 - linear_val) * ((linear_val - 0.5) * 2) ** 0.2)
-            return f"{round(adjusted_val * 100, 2)}%"
-
+    @log.catch
     def process_frame(self, frame):
         """Обробка кадру: зменшення розміру, перетворення в RGB, пошук облич."""
         small_frame = cv2.resize(frame, (0, 0), fx=1 / SCALE_FACTOR, fy=1 / SCALE_FACTOR)
@@ -52,14 +57,17 @@ class FaceRecognition:
 
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
-                confidence = self.face_confidence(face_distances[best_match_index])
+                # confidence = self.face_confidence(face_distances[best_match_index])
 
-            face_names.append(f"{name} ({confidence})")
+            # face_names.append(f"{name} ({confidence})")
+            face_names.append(name)
 
         # Малювання рамок навколо облич і виведення імені
         for (top, right, bottom, left), name in zip(face_locations, face_names):
             top, right, bottom, left = [int(coord * SCALE_FACTOR) for coord in (top, right, bottom, left)]
             color = (0, 0, 255) if name.startswith("Unknown") else (0, 255, 0)  # Червоний для невідомих, зелений для знайомих
+
+            # cv2.imwrite(f"faces/{datetime.datetime.now().strftime('%H-%M-%S-%f')}.jpg", frame[top:bottom, left:right])
 
             cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
             cv2.rectangle(frame, (left, top - 35), (right, top), color, -1)
@@ -69,9 +77,10 @@ class FaceRecognition:
 
         return frame
 
+    @log.catch
     def run_recognition(self):
         """Запуск відеопотоку та розпізнавання облич."""
-        vid = cv2.VideoCapture(VIDEO_SOURCE)
+        vid = cv2.VideoCapture()
 
         if not vid.isOpened():
             raise ValueError("Відео не знайдено або не може бути відкрито!")
@@ -79,14 +88,14 @@ class FaceRecognition:
         while True:
             ret, frame = vid.read()
             if not ret:
+                log.error("Відео не знайдено або не може бути відкрито!")
                 break
-
-            x, y, w, h = ROI  # Визначення області інтересу (ROI)
-            frame = frame[y:y + h, x:x + w]  # Обрізка кадру за ROI
 
             if int(vid.get(cv2.CAP_PROP_POS_FRAMES)) % FRAME_SKIP == 0:
                 frame = self.process_frame(frame)  # Обробка кадру
                 cv2.imshow("Face Recognition", frame)  # Відображення кадру
+
+            # cv2.waitKey(0)
 
             if cv2.waitKey(1) == ord("q"):  # Вихід при натисканні 'q'
                 break
